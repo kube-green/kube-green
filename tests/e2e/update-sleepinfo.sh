09b0291d7694e7c1a0a0fa292ac221bac01fb060
Davide Bianchi <bianchidavide12@gmail.com>
2022-02-21 02:17:19 +0100

    test: add e2e test step

---
+++ tests/e2e/update-sleepinfo.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+set -o errexit
+set -o nounset
+
+# Set default umask for created files inside the script
+umask 077
+
+# Check if sed is installed
+command -v sed >/dev/null 2>&1 || { echo "sed must be installed for this script! Aborting"; exit 1; }
+# check if date is installed
+command -v date >/dev/null 2>&1 || { echo "date must be installed for this script! Aborting"; exit 1; }
+# check if kubectl is installed
+command -v kubectl >/dev/null 2>&1 || { echo "kubectl must be installed for this script! Aborting"; exit 1; }
+
+cp ../sleepinfo.yaml /tmp/sleepinfo.yaml
+
+unamestr=$(uname)
+
+if [[ $unamestr == 'Darwin' ]]; then
+  sed -i.bck "s|sleepAt: \"20:00\"|sleepAt: \"$(date -v+1M +"%H:%M")\"|g" "/tmp/sleepinfo.yaml"
+  sed -i.bck "s|wakeUpAt: \"08:00\"|wakeUpAt: \"$(date -v+2M +"%H:%M")\"|g" "/tmp/sleepinfo.yaml"
+else
+  sed -i.bck "s|sleepAt: \"20:00\"|sleepAt: \"$(date -d '1 minutes' +"%H:%M")\"|g" "/tmp/sleepinfo.yaml"
+  sed -i.bck "s|wakeUpAt: \"08:00\"|wakeUpAt: \"$(date -d '2 minutes' +"%H:%M")\"|g" "/tmp/sleepinfo.yaml"
+fi
+
+kubectl apply -f /tmp/sleepinfo.yaml -n $NAMESPACE
