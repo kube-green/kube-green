ccc0d019cbbadeb42524f3caa8177511e5d17a53
Davide Bianchi <bianchidavide12@gmail.com>
2022-08-21 19:25:23 +0200

    test: add e2e test for only cronjobs and only deploy

--- tests/e2e/update-sleepinfo.sh
+++ tests/e2e/update-sleepinfo.sh
@@ -13,18 +13,20 @@ command -v date >/dev/null 2>&1 || { echo "date must be installed for this scrip
 # check if kubectl is installed
 command -v kubectl >/dev/null 2>&1 || { echo "kubectl must be installed for this script! Aborting"; exit 1; }
 
-cp ../sleepinfo.yaml /tmp/sleepinfo.yaml
+filename=$1
+
+cp ../$filename /tmp/$filename
 
 unamestr=$(uname)
 
 if [[ $unamestr == 'Darwin' ]]; then
-  sed -i.bck "s|sleepAt: \"20:00\"|sleepAt: \"$(date -u -v+1M +"%H:%M")\"|g" "/tmp/sleepinfo.yaml"
-  sed -i.bck "s|wakeUpAt: \"08:00\"|wakeUpAt: \"$(date -u -v+2M +"%H:%M")\"|g" "/tmp/sleepinfo.yaml"
+  sed -i.bck "s|sleepAt: \"20:00\"|sleepAt: \"$(date -u -v+1M +"%H:%M")\"|g" "/tmp/$filename"
+  sed -i.bck "s|wakeUpAt: \"08:00\"|wakeUpAt: \"$(date -u -v+2M +"%H:%M")\"|g" "/tmp/$filename"
 else
-  sed -i.bck "s|sleepAt: \"20:00\"|sleepAt: \"$(date -u -d '1 minutes' +"%H:%M")\"|g" "/tmp/sleepinfo.yaml"
-  sed -i.bck "s|wakeUpAt: \"08:00\"|wakeUpAt: \"$(date -u -d '2 minutes' +"%H:%M")\"|g" "/tmp/sleepinfo.yaml"
+  sed -i.bck "s|sleepAt: \"20:00\"|sleepAt: \"$(date -u -d '1 minutes' +"%H:%M")\"|g" "/tmp/$filename"
+  sed -i.bck "s|wakeUpAt: \"08:00\"|wakeUpAt: \"$(date -u -d '2 minutes' +"%H:%M")\"|g" "/tmp/$filename"
 fi
 
-cat /tmp/sleepinfo.yaml
+cat /tmp/$filename
 
-kubectl apply -f /tmp/sleepinfo.yaml -n $NAMESPACE
+kubectl apply -f /tmp/$filename -n $NAMESPACE
