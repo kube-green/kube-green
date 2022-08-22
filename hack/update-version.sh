154dd45d164654f29759446c08be2f92768a8614
Davide Bianchi <bianchidavide12@gmail.com>
2022-08-22 10:38:57 +0200

    docs: CHANGELOG now links to releases

--- hack/update-version.sh
+++ hack/update-version.sh
@@ -19,8 +19,7 @@ else
   NOW_DATE="$(date -u -I)"
 fi
 
-sed -i.bck "s|## Unreleased|## ${TAG_VALUE} - ${NOW_DATE}|g" "${SOURCE_DIR}/CHANGELOG.md"
 sed -i.bck -E "s|VERSION \?= [0-9]+.[0-9]+.[0-9]+.*|VERSION ?= ${TAG_VALUE_WITHOUT_V}|" "${SOURCE_DIR}/Makefile"
 sed -i.bck -E "s|newTag: [0-9]+.[0-9]+.[0-9]+.*|newTag: ${TAG_VALUE_WITHOUT_V}|" "${SOURCE_DIR}/config/manager/kustomization.yaml"
 sed -i.bck -E "s|containerImage: kubegreen/kube-green:[0-9]+.[0-9]+.[0-9]+.*|containerImage: kubegreen/kube-green:${TAG_VALUE_WITHOUT_V}|" "${SOURCE_DIR}/config/manifests/bases/kube-green.clusterserviceversion.yaml"
-rm -fr "${SOURCE_DIR}/CHANGELOG.md.bck" "${SOURCE_DIR}/Makefile.bck" "${SOURCE_DIR}/config/manager/kustomization.yaml.bck" "${SOURCE_DIR}/config/manifests/bases/kube-green.clusterserviceversion.yaml.bck"
+rm -fr "${SOURCE_DIR}/Makefile.bck" "${SOURCE_DIR}/config/manager/kustomization.yaml.bck" "${SOURCE_DIR}/config/manifests/bases/kube-green.clusterserviceversion.yaml.bck"
