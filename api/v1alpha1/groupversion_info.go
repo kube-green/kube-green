d8fc2d34f8c72f91d01e9b2f982cf24887a87642
Davide Bianchi <bianchidavide12@gmail.com>
2022-10-05 09:53:38 +0200

    fix: add kube-green selector to manifests

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -3,8 +3,8 @@ Copyright 2021.
 */
 
 // Package v1alpha1 contains API Schema definitions for the v1alpha1 API group.
-//+kubebuilder:object:generate=true
-//+groupName=kube-green.com
+// +kubebuilder:object:generate=true
+// +groupName=kube-green.com
 package v1alpha1
 
 import (
