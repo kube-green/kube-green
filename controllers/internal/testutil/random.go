269bb36e1337a9ecda0bb88159265d7f353c0cb2
Davide Bianchi <bianchidavide12@gmail.com>
2022-10-24 21:13:29 -0400

    fix: fix random data generation

--- controllers/internal/testutil/random.go
+++ controllers/internal/testutil/random.go
@@ -1,13 +1,21 @@
 package testutil
 
-import "math/rand"
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+)
 
 const letterBytes = "abcdefghijklmnopqrstuvwxyz"
 
 func RandString(n int) string {
 	b := make([]byte, n)
 	for i := range b {
-		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+		random, err := rand.Int(rand.Reader, big.NewInt(int64(len(letterBytes))))
+		if err != nil {
+			panic(fmt.Errorf("random error: %s", err))
+		}
+		b[i] = letterBytes[random.Int64()]
 	}
 	return string(b)
 }
