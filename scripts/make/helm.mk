fe6509141157fcfc135b63f5778153816438a6c5
Wen Zhou <wenzhou@redhat.com>
2025-10-29 18:37:58 +0100

    fix: remove .EXPORT_ALL_VARIABLES in Makefile, only export needed env. (#618)
    - also remove SHELL in hel.mk as it has been set in Makefile
    
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>

--- scripts/make/helm.mk
+++ scripts/make/helm.mk
@@ -2,7 +2,6 @@ HELM_TMPL_CMD ?= helm template -f charts/kube-green/values.yaml -f charts/kube-g
 HELM_TMPL_OUT ?= .helm.template-output.yaml
 HELM_SNAPSHOT_OUT ?= charts/snapshots/test-output.snap.yaml
 tmpl_debug ?=
-SHELL=/usr/bin/env bash
 
 ##@ Helm Chart utilities
 .PHONY: chart-snapshot
